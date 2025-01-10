bd8e090a6c4f279ede705204b432a9d30508a15f
Lyubov Shamanina <82117621+sha-ma-ni@users.noreply.github.com>
2025-01-10 10:48:04 +0100

    Kommentare hinzugefügt

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,13 +10,15 @@ import {Component, OnInit} from "@angular/core";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  hide = true;
+  hide = true; //Password sichtbar/unsichtbar
   loggedIn = false;
+  
+ // Initialisierung des Formulars mit E-Mail und Passwort
   loginForm = this.fb.group({
     email: [null, Validators.required],
     password: [null, Validators.required]
   });
-  warning: boolean;
+  warning: boolean; //zeig,ob Fehlermeldung biem Login angezeigt werden soll
 
   constructor(
     private fb: FormBuilder,
@@ -26,16 +28,20 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
+  // Überprüfen, ob das Formular gültig ist, bevor die Anfrage gesendet wird
   onSubmit(): void {
+    // die Werte aus dem Formular extrahieren
     const values = this.loginForm.value;
     const email = values.email;
     const password =  values.password;
     console.log('values email', email)
     console.log('values password', password)
 
+    //LoginUser-Methode aufrufen, um Authentifizirung durchzuführen
     this.auth.loginUser(email!, password!).subscribe({
         next: (response) => {
-          this.warning = false;
+          this.warning = false; //deaktiviert, wenn Login erfolgreich
           this.auth.login(response);
           this.loggedIn = true;
           console.log('login response',response);
